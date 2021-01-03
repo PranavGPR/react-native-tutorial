e03e68a1168227856a38ddfc21ae86a0dd0c3fca
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-03 19:34:52 +0530

    flex props practiced

--- App.js
+++ App.js
@@ -10,16 +10,18 @@ export default function App() {
         flexDirection: "row",
         justifyContent: "center",
         alignItems: "center",
-        alignContent: "center",
-        flexWrap: "wrap",
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 400,
+          flex: -1,
+          height: 100,
+        }}
       />
       <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
-      <View style={{ backgroundColor: "gray", width: 100, height: 100 }} />
     </View>
   );
 }
