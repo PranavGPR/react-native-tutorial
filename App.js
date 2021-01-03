5fe0b23946199302f7db39f4c014c22927b44a42
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-03 22:00:44 +0530

    icons usage practiced

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -8,7 +9,7 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>I love React Native!</AppText>
+      <MaterialCommunityIcons name='email' size={200} color='dodgerblue' />
     </View>
   );
 }
