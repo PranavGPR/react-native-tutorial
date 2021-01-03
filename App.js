700761b7767b74413d3a5b32254b851190b0e1fc
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-03 21:57:48 +0530

    font encapsulation practiced

--- App.js
+++ App.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+
+import AppText from "./app/components/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        style={{
-          fontSize: 30,
-          // fontFamily: "Courier",
-          fontWeight: "bold",
-          fontStyle: "italic",
-          color: "tomato",
-          textTransform: "capitalize",
-          textAlign: "center",
-          lineHeight: 50,
-        }}
-      >
-        I love React Native! This is my first React Native App! This is extra
-        words
-      </Text>
+      <AppText>I love React Native!</AppText>
     </View>
   );
 }
