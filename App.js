2dc3f62412f691777308e690e57df3098e03b454
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-03 21:50:26 +0530

    changing default fonts practiced

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 import React from "react";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
+          fontSize: 30,
+          // fontFamily: "Courier",
+          fontWeight: "bold",
+          fontStyle: "italic",
+          color: "tomato",
+          textTransform: "capitalize",
+          textAlign: "center",
+          lineHeight: 50,
         }}
       >
-        <View style={{ backgroundColor: "gold", width: 50, height: 50 }}></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          margin: 20,
-        }}
-      />
+        I love React Native! This is my first React Native App! This is extra
+        words
+      </Text>
     </View>
   );
 }
