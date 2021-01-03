6c5e5aca233bb14f1122d1fe874b4f41150ddc24
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-03 19:28:36 +0530

    alignment practiced

--- App.js
+++ App.js
@@ -3,11 +3,19 @@ import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
