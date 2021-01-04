358df6476abc90f819db0acc891487f5a54899e2
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-04 11:52:57 +0530

    ListItem code refactored

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
 
 import AppText from "./AppText";
 
@@ -8,8 +9,8 @@ export default function ListItem({ title, image, subtitle }) {
     <View style={styles.container}>
       <Image style={styles.image} source={image} />
       <View style={styles.details}>
-        <AppText>{title}</AppText>
-        <AppText style={{ color: "grey" }}>{subtitle}</AppText>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subtitle}>{subtitle}</AppText>
       </View>
     </View>
   );
@@ -21,12 +22,18 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
   },
+  details: {
+    marginHorizontal: 10,
+  },
   image: {
     width: 75,
     height: 75,
     borderRadius: 50,
   },
-  details: {
-    marginHorizontal: 10,
+  subtitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: "500",
   },
 });
