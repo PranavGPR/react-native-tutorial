ce0666251c85d262b7427214c02ad425ca43701c
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-06 17:17:14 +0530

    ImageInput component selecting and deleting image functionality added

--- App.js
+++ App.js
@@ -1,35 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions";
-import { Button, Image, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
 import Screen from "./app/components/Screen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState("");
-
-  const requestPermission = async () => {
-    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access");
-  };
-
-  useEffect(() => {
-    requestPermission();
-  }, []);
+  const [imageUri, setImageUri] = useState();
 
   const selectImage = async () => {
     try {
       const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) setImageUri(result.uri);
+      if (!result.cancelled) {
+        setImageUri(result.uri);
+      }
     } catch (error) {
       console.log("Error reading an image");
     }
   };
 
   return (
-    <Screen>
-      <Button title='Select Image' onPress={selectImage} />
-      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    <Screen style={styles.container}>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
     </Screen>
   );
 }
---
+++ app/components/ImageInput.js
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import {
+  Image,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+  Alert,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+export default function ImageInput({ imageUri, onChangeImage }) {
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access");
+  };
+
+  const handlePress = () => {
+    if (!imageUri) {
+      selectImage();
+    } else {
+      Alert.alert("Delete", "Are you sure you want to delete this image?", [
+        { text: "Yes", onPress: () => onChangeImage(null) },
+        { text: "No" },
+      ]);
+    }
+  };
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 0.5,
+      });
+      if (!result.cancelled) {
+        onChangeImage(result.uri);
+      }
+    } catch (error) {
+      console.log("Error reading an image");
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            name='camera'
+            color={colors.medium}
+            size={40}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.lightgrey,
+    borderRadius: 15,
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 100,
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
