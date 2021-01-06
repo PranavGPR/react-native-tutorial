b2929e8a260bd91f4a628dbc5d045ed00c31753e
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-06 16:30:41 +0530

    Asking Permissions using Permissions API practiced

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, { useEffect } from "react";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 import { StyleSheet } from "react-native";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const requestPermisson = async () => {
+  const requestPermission = async () => {
+    const result = await Permissions.askAsync(
+      Permissions.MEDIA_LIBRARY,
+      Permissions.LOCATION
+    );
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
     if (!granted) alert("You need to enable permission to access");
   };
 
   useEffect(() => {
-    requestPermisson();
+    requestPermission();
   }, []);
 
   return <Screen></Screen>;
