6f7f255f2bc469b3c0cdac3b0ad97678b2c35d5d
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-06 16:27:41 +0530

    Permission access for image using ImagePicker practiced

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as ImagePicker from "expo-image-picker";
 import { StyleSheet } from "react-native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermisson = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access");
+  };
+
+  useEffect(() => {
+    requestPermisson();
+  }, []);
+
+  return <Screen></Screen>;
 }
 
 const styles = StyleSheet.create({});
