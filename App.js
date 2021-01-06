ffe87322fdebacfa8938c8abb4894ef28dedbd69
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-01-06 22:01:45 +0530

    Page navigation learnt and practiced

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Button, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import Screen from "./app/components/Screen";
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title='View Tweet'
+      onPress={() => navigation.navigate("TweetDetails")}
+    />
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>TweetDetails</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name='Tweets' component={Tweets}></Stack.Screen>
+    <Stack.Screen name='TweetDetails' component={TweetDetails}></Stack.Screen>
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({});
